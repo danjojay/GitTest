66eae551b472adf5a7033ab14c6c185fb2c2cf2e
danjojay <danieljoshua309@gmail.com>
2018-02-25 18:04:15 +0530

    full Commit

---
+++ src/test/java/GITTest/GITTest.java
@@ -0,0 +1,27 @@
+package GITTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GITTest {
+	
+	@Test
+	public void loginTest() throws IOException {
+		
+		WebDriver wd = new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com");
+		
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE); 
+        FileUtils.copyFile(src, new File("C:\\Users\\daniyal\\eclipse-workspace\\GITTest\\screenshot\\google.jpeg"));
+
+	}
+
+}
